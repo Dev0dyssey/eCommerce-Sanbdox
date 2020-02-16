40c2a2a3774bf918073d04a3f91c1df07de6321d
ThomasMartinUK <tmuk@protonmail.com>
2020-02-16 12:55:01 +0000

    Added React Router and comments on Router Switch to the App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
 import "./App.css";
 
+import { Switch, Route } from "react-router-dom";
+
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* SWITCH RENDERS THE VERY FIRST PATH IT MATCHES AND NOTHING ELSE */}
+      {/* IN THE CASE BELOW "/" IS THE FIRST MATCHING ROUTE, SO "/HATS" WILL NOT BE RENDERED */}
+      {/* EXACT PARAMETER STILL ALLOWS TO NAVIGATE TO THE SPECIFIC PATH */}
+      {/* SWITCH IS A GOOD WAY TO FOLLOW A LOGICAL ROUTING PATTERN */}
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
