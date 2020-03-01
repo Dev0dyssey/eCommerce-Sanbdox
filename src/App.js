c44b502941409b6c323b1cad5b1dfe73e17d0d8a
ThomasMartinUK <tmuk@protonmail.com>
2020-03-01 08:27:57 +0000

    Added new Header component to the page along with fixing Font Awesome not importing correctly. Header now navigates to various parts of the App

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       {/* IN THE CASE BELOW "/" IS THE FIRST MATCHING ROUTE, SO "/HATS" WILL NOT BE RENDERED */}
       {/* EXACT PARAMETER STILL ALLOWS TO NAVIGATE TO THE SPECIFIC PATH */}
       {/* SWITCH IS A GOOD WAY TO FOLLOW A LOGICAL ROUTING PATTERN */}
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
