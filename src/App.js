24f0f081be03d56a9306a084fb242813788991d1
ThomasMartinUK <tmuk@protonmail.com>
2020-03-08 09:34:49 +0000

    Added custom reusable components to the App

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "./App.css";
 
 import { Switch, Route } from "react-router-dom";
 
+import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sing-in-and-sign-up/sing-in-and-sign-up.component";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
