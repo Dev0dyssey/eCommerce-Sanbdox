51c5864f2f8905100c3e3e242527c0b136e05da4
ThomasMartinUK <tmuk@protonmail.com>
2020-04-16 18:50:15 +0100

    Added Redux selector to show number of items selected inside the cart icon inside the App

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,10 +7,10 @@ import { ReactComponent as ShoppingIcon } from "../../assets/cart.svg";
 
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
@@ -18,7 +18,16 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  //The below reducer is also a selector
+  //Selector gets the entire state and slices/selects a small portion of that state to be used
+  itemCount: cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+    0
+  ),
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CartIcon);
