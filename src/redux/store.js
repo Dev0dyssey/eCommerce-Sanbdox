078a5b36a13fa42a216a99350609ff150a2eb1cd
ThomasMartinUK <tmuk@protonmail.com>
2020-04-24 19:50:44 +0100

    Added the Redux-Persist library to allow session storage and persistence within the App

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
@@ -6,6 +7,8 @@ const middleWares = [logger];
 
 // ...middleWares and middleWares = [logger] allow for a greater degree of scalability
 // Easier to add new property to the middleWares array[] when we want to pass more arguments into the applyMiddleware() method
-const store = createStore(rootReducer, applyMiddleware(...middleWares));
+export const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
