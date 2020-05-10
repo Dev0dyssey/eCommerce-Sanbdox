51476531e9cef670f6bf761c29fbaa7cd060a0ad
ThomasMartinUK <tmuk@protonmail.com>
2020-05-10 09:29:51 +0100

    Removing Redux logger from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,11 @@ import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
-const middleWares = [logger];
+const middleWares = [];
+
+if (process.env.NODE_ENV === "development") {
+  middleWares.push(logger);
+}
 
 // ...middleWares and middleWares = [logger] allow for a greater degree of scalability
 // Easier to add new property to the middleWares array[] when we want to pass more arguments into the applyMiddleware() method
