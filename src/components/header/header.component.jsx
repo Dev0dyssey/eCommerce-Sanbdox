6f6df034ab1dea0adb497560c9d654be75d12d76
ThomasMartinUK <tmuk@protonmail.com>
2020-05-11 07:03:45 +0100

    Added styled components library to allos CSS in JS, began rewriting some of the CSS stylesheets to use Styled Components inside the App

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
@@ -12,34 +12,34 @@ import { selectCurrentUser } from "../../redux/user/user.selectors";
 // Specific React syntax to import SVG images
 import { ReactComponent as Logo } from "../../assets/crown_logo.svg";
 
-import "./header.styles.scss";
+// import "./header.styles.scss";
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionContainer,
+  OptionDiv,
+  OptionLink,
+} from "./header.styles";
 
 const Header = ({ currentUser, hidden }) => (
-  <div className="header">
-    <Link className="logoContainer" to="/">
+  <HeaderContainer>
+    <LogoContainer to="/">
       <Logo className="logo" />
-    </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
-        SHOP
-      </Link>
-      <Link className="option" to="/shop">
-        CONTACT
-      </Link>
+    </LogoContainer>
+    <OptionContainer>
+      <OptionLink to="/shop">SHOP</OptionLink>
+      <OptionLink to="/shop">CONTACT</OptionLink>
       {currentUser ? (
-        <div className="option" onClick={() => auth.signOut()}>
-          {" "}
-          SIGN OUT
-        </div>
+        <OptionDiv onClick={() => auth.signOut()}> SIGN OUT</OptionDiv>
       ) : (
-        <Link className="option" to="/signin">
+        <OptionLink className="option" to="/signin">
           SIGN IN
-        </Link>
+        </OptionLink>
       )}
       <CartIcon />
-    </div>
+    </OptionContainer>
     {hidden ? null : <CartDropdown />}
-  </div>
+  </HeaderContainer>
 );
 
 // {value: { value }} a way to destructure nested components
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Directory from "../../components/directory/directory.component";
 
-import "./homepage.style.scss";
+import { HomePageContainer } from "./homepage.styles";
 
 const HomePage = () => (
   // IF WE DO NOT NEED LOGIC WE CAN "RETURN" DIRECTLY INSIDE THE METHOD
   // ALLOWS FOR METHOD = () => (CONTENT)
   // ALTERNATIVE TO METHOD = () => {LOGIC---RETURN(CONTENT)}
-  <div className="homepage">
+  <HomePageContainer>
     <Directory />
-  </div>
+  </HomePageContainer>
 );
 
 export default HomePage;
