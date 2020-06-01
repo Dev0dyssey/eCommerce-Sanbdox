6c3f54de93d39c9b4d3100e85b43936707b0d112
ThomasMartinUK <tmuk@protonmail.com>
2020-06-01 18:47:29 +0100

    Refactor of Google and Email Sign In method to use Redux-Sagas inside the App

--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -9,16 +9,14 @@ const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     // Stacking the action types allows us to specify that if either of these actions are called, the code block below should run
     // Eliminates the need to rewrite the entire code block with almost exact same code (adhering to DRY principles)
-    case UserActionTypes.GOOGLE_SIGN_IN_SUCCESS:
-    case UserActionTypes.EMAIL_SIGN_IN_SUCCESS:
+    case UserActionTypes.SIGN_IN_SUCCESS:
       return {
         ...state,
         currentUser: action.payload,
         error: null,
       };
 
-    case UserActionTypes.GOOGLE_SIGN_IN_FAILURE:
-    case UserActionTypes.EMAIL_SIGN_IN_FAILURE:
+    case UserActionTypes.SIGN_IN_FAILURE:
       return {
         ...state,
         error: action.payload,
