0a36c77ff6eb553a09f5853c7cf23732ff7fa9bd
ThomasMartinUK <tmuk@protonmail.com>
2020-06-13 08:49:38 +0100

    CollectionPage

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 
 import CollectionItem from "../../components/collection-item/collection-item.component";
 
 import { selectCollection } from "../../redux/shop/shop.selectors";
 
+import { firestore } from "../../firebase/firebase.utils";
+
 import "./collection.styles.scss";
 
 const CollectionPage = ({ collection }) => {
+  useEffect(() => {
+    console.log("I am subscribing");
+    const unsubscribeFromCollections = firestore
+      .collection("collections")
+      .onSnapshot((snapshot) => console.log(snapshot));
+    // When we return a method from useEffect (a clean up method) we replicate the usage of componentWillUnmount() lifecycle hook
+    return () => {
+      console.log("I am unsubscribing");
+      unsubscribeFromCollections();
+    };
+  }, []);
+
   const { title, items } = collection;
 
   return (
